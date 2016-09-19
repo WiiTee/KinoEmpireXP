54877a3720365c88aac25e2f301c6b6317bd4dfe
lwroslundh <lwormz@gmail.com>
2016-09-19 12:05:52 +0200

    test

--- src/Test.java
+++ src/Test.java
@@ -5,9 +5,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Test");
-<<<<<<< HEAD
         System.out.println("Test2");
-=======
->>>>>>> test
+
     }
 }
