218fc0853a58a76b46f18a0bdf7efb72ef80a43b
lwroslundh <lwormz@gmail.com>
2016-09-20 12:43:00 +0200

    JavaFX started

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,20 @@
 /**
  * Created by Lasse on 19/09/16.
  */
-public class Test
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application
 {
 
     public static void main(String[] args)
     {
-
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage)
+    {
 
-
+    }
 }
