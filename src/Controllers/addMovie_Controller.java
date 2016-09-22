512fba20b50aa9f27add9ad372c4eb989a043e69
christianflemmer <flemmer94@hotmail.com>
2016-09-22 10:09:17 +0200

    Controller-klasser oprettet til hver .fxml fil, der har til hensigt at håndtere funktionaliteten fra brugergrænsefladen individuelt

---
+++ src/Controllers/addMovie_Controller.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+/**
+ * Created by Flemmer on 22-09-2016.
+ */
+public class addMovie_Controller {
+
+
+
+}
---
+++ src/Controllers/tickets1view_Controller.java
@@ -0,0 +1,7 @@
+package Controllers;
+
+/**
+ * Created by Flemmer on 22-09-2016.
+ */
+public class tickets1view_Controller {
+}
