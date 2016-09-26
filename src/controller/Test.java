8439fdb3ac17f080fc0fa74f03b3be320d21f619
BogiHammer <bogihammer@icloud.com>
2016-09-26 12:54:22 +0200

    Bogi

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -16,13 +16,15 @@ public class Test extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+
+
         ViewOrder viewOrder = new ViewOrder();
 
         Parent root = FXMLLoader.load(getClass().getResource("/view/customerOrder.fxml"));
         primaryStage.setTitle("Kunde Ordre");
         primaryStage.setScene(new Scene(root, 1366, 768));
         primaryStage.show();
-        viewOrder.updateTableView();
+//        viewOrder.updateTableView();
     }
 
 
